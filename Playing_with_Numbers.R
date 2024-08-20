cca9548ba187ff31ac6acc7e69fd62ffd8cf4d07
Sheila Mwangi <mwangisheila838@gmail.com>
2024-08-20 03:42:21 +0300

    Random

---
+++ Playing_with_Numbers.R
@@ -0,0 +1,10 @@
+a<-c(200,300,400)
+b<-c(10,50,60)
+m<-a-b
+my_sqrt<-sqrt(m-1)
+my_div<-my_sqrt/2
+my_mult<-my_div*3
+#a sequence of 3 numbers between 15 and 20
+my_seq<-seq(15,20,length=3)
+my_add<-my_sqrt+my_div+my_mult
+my_add
